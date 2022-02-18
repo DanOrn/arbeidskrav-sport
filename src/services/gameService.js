2e2df4c9441f0ed91bea79d4cda16d23ce04050b
unknown <daniel.s.ornevik@gmail.com>
2022-02-18 15:54:49 +0100

    lagt til bilder

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,11 +1,11 @@
 const gameService = ( function(){  
     const games = ([
-        {title: "Cyberpunk 2077", image: "cyberpunk2077.png", childFriendly: false},
-        {title: "DOTA 2", image: "dota2.png", childFriendly: false},
-        {title: "League Of Legends", image: "lol.png", childFriendly: false},
-        {title: "Starcraft 2", image: "starcraft2.png", childFriendly: false},
-        {title: "Zelda A Link's Awakening", image: "zelda.png", childFriendly: true},
-        {title: "Animal Crossing", image: "animalCrossing.png", childFriendly: true}
+        {title: "Cyberpunk 2077", image: "pilogbue.png", childFriendly: false},
+        {title: "DOTA 2", image: "klatring.png", childFriendly: false},
+        {title: "League Of Legends", image: "pingpong.png", childFriendly: false},
+        {title: "Starcraft 2", image: "esport.png", childFriendly: false},
+        {title: "Zelda A Link's Awakening", image: "fotball.png", childFriendly: true},
+        {title: "Animal Crossing", image: "kurling.png", childFriendly: true}
     ]);
 
     const getAll = () => {
