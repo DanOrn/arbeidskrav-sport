10aea44b09bfcff1df7c0bd9b5b326bcb0497fc4
unknown <daniel.s.ornevik@gmail.com>
2022-02-22 22:12:44 +0100

    Endret navn

---
+++ src/pages/SportSearch.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <add-sport></add-sport>
+    </section>
+</template>
+<script>
+import AddSport from"../components/sports/AddSport.vue"
+
+export default {
+    components: {
+        AddSport
+    }
+}
+</script>
